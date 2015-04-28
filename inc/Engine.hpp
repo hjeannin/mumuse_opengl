ef1c12157257f61b9688a325dd6eba30117cef03
hjeannin <hjeannin@student.42.fr>
2015-04-28 21:57:43 +0200

    all project

---
+++ inc/Engine.hpp
@@ -0,0 +1,58 @@
+#ifndef ENGINE_HPP
+# define ENGINE_HPP
+
+# include <iostream>
+# include <unistd.h>
+# include "Camera.hpp"
+
+# define VTAB 5000 // vertex_tab size
+# define PARTY_START	36
+# define PARTY_SIZE		300
+
+class Engine
+{
+private:
+	int _status;
+
+public:
+	SDL_Window			*window;
+	SDL_GLContext		context;
+	Camera				*camera;
+	int					x_res;
+	int					y_res;
+	float				*vertex_tab;
+	bool				moving;
+	int					fov;
+	double				z_near;
+	double				z_far;
+
+	Engine(void);
+	Engine(const Engine &src);
+	Engine(int status);
+	~Engine(void);
+	int getStatus(void) const;
+	void setStatus(int status);
+	Engine &operator=(Engine const &rhs);
+
+	int		init(void);
+	void	loop(void);
+	void	render(void);
+	void	renderAxes(void);
+	void	renderShape(void);
+	void	renderGround(void);
+	void	switchLight(bool l0, bool l1, bool l2);
+	void	moveYourBody(int start, int size);
+	void	makeWTF(int start, int size);
+	void	makeCube(float s);
+	void	renderTriangleArray(int start, int size);
+	void	renderPointArray(int start, int size);
+	int		sdlError(int code);
+	void	setFOV(int new_fov);
+	void	initLight(void);
+	bool	randomBool(void);
+	void	randomLight(void);
+};
+
+std::ostream &operator<<(std::ostream &o, Engine const &i);
+
+#endif
